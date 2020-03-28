dbc6b60e0af704bbb8761e931d5d3439935019d4
Sharon Jin <sharonjin@Sharons-MBP.home>
2020-03-28 19:25:04 -0400

    About Us layout

---
+++ src/pages/about-us.js
@@ -0,0 +1,41 @@
+import React from "react"
+
+import Template from "../components/template";
+
+const IndexPage = () => (
+  <Template>
+    <h3>About Us</h3>
+      <div>
+          We're an all-inclusive cool peoples. hi
+      </div>
+    <h3>Our Mission</h3>
+      <div>
+          Even though the Columbia and Barnard students are now spread out all over the world, we are still a
+          community - here to support, engage, and enrich each other’s lives. In light of recent events, most of the
+          normal campus activities have been suspended, but we can still maintain our community through virtual campus
+          experiences.<br/><br/>
+
+          Join us April 4-6 for a three-day design challenge to discover, design, and build solutions for the
+          challenges of remote living. Being remote poses big challenges to mental health, motivation, concentration,
+          and seeking help. Many of us worry about the present as well as the near and far-term future.<br/><br/>
+
+          Let’s come together to find ways to:
+          <ol>
+              <li>Help students cope with social isolation (eg. fun activities, communication portals, articles,
+          videos, etc) </li>
+              <li>Help students continue to learn, work and live to the best of their abilities.</li>
+              <li>Maintain and adapt campus activities that support the community.</li>
+          </ol>
+
+          Students from all majors are welcome to participate. No experience necessary. We have faculty and student
+          mentors to help you get your ideas off the ground. If you have an idea for an app, we can connect you to
+          students eager to build things.<br/><br/>
+
+          To learn more about the website, check out our group: <a href="https://bit.ly/virtual-campus-group">https://bit.ly/virtual-campus-group</a><br/>
+          To RSVP or get more information about the event: <a href="https://bit.ly/design-challenge-rsvp">https://bit.ly/design-challenge-rsvp</a><br/>
+          Also, please join the Slack to get updates and share your ideas: <a href="https://bit.ly/virtual-campus-slack">https://bit.ly/virtual-campus-slack</a>
+      </div>
+  </Template>
+);
+
+export default IndexPage
