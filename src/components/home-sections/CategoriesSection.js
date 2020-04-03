bb3714d8ebc26b38929665f6eb833a793d04234e
tejitpabari99 <tvp2107@columbia.edu>
2020-04-03 17:47:47 +0530

    Layout Changes

--- src/components/home-sections/CategoriesSection.js
+++ src/components/home-sections/CategoriesSection.js
@@ -1,12 +1,18 @@
 import GridItem from "../material-kit-components/Grid/GridItem";
 import CustomCard1 from "../CustomCard1";
 import GridContainer from "../material-kit-components/Grid/GridContainer";
+import Typography from "@material-ui/core/Typography";
 import React from "react";
+import {Link} from "gatsby";
+import Card from "../material-kit-components/Card/Card.js";
+import CardBody from "../material-kit-components/Card/CardBody.js";
 
 import styles from "../../assets/material-kit-assets/jss/material-kit-react/views/landingPage.js";
 import {makeStyles} from "@material-ui/core/styles";
 import Data from "../../assets/Data";
+import CustomCard2 from "../CustomCard2";
 const CampusData = Data.CampusData;
+const IndexCampusData = Data.IndexCampusData;
 
 const useStyles = makeStyles(styles);
 const containerStyles = makeStyles(() => ({
@@ -14,7 +20,27 @@ const containerStyles = makeStyles(() => ({
     paddingTop: "20px"
   },
   gridEle: {
-    marginBottom: "30px"
+    marginBottom: "40px",
+    marginTop: 5
+  },
+  title: {
+    textTransform: "capitalize",
+    display: 'inline-block'
+  },
+  seeAll: {
+    float:'right',
+    display: 'inline-block',
+    textDecoration: 'underline'
+  },
+  seeAllLink: {
+    color: '#3838ff',
+    "&:hover":{
+      color: 'blue',
+      textDecoration: 'underline'
+    },
+    float:'right',
+    display: 'inline-block',
+    textDecoration: 'underline'
   }
 }));
 
@@ -23,17 +49,35 @@ export default function CategoriesSection() {
   const contStyle = containerStyles();
   const classes = useStyles();
   return (
-    <GridContainer>
-      {CampusData.map((ele) => {
+    <div>
+      {Object.keys(IndexCampusData).map(key => {
         return (
-          <GridItem xs={12} sm={6} md={4} className={contStyle.gridEle}>
-            <CustomCard1 imgURL={ele.imgURL}
-                         title={ele.title}
-                         linkURL={ele.pageURL.toLowerCase()}
-            />
-          </GridItem>
+          <div>
+            <Typography variant="h6" component="h2" className={contStyle.title}>{CampusData[key]['title']}</Typography>
+            <Link to={CampusData[key]['pageURL']} className={contStyle.seeAllLink}>See All</Link>
+            <GridContainer>
+              {IndexCampusData[key].map(ele => {
+                let splt = ele.split('/');
+                let data = CampusData[key];
+                for (let k of splt){ data = data['data'][k]}
+                return (
+                  <GridItem xs={6} sm={4} md={3} className={contStyle.gridEle}>
+                    <CustomCard2
+                      website={data.website || data.facebook || data.instagram }
+                      imgURL={data.imgURL}
+                      title={data.title}
+                      description={data.description}
+                      iosLink={data.iosLink}
+                      androidLink={data.androidLink}
+                      share
+                    />
+                  </GridItem>
+                )
+              })}
+            </GridContainer>
+          </div>
         )
       })}
-    </GridContainer>
+    </div>
   )
 }
