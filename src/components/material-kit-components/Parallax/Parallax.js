dcbc8dd539177a55905701b0941b4c55d88f132e
tejitpabari99 <35284488+tejitpabari99@users.noreply.github.com>
2020-04-04 16:50:52 +0530

    build changes (#11)

--- src/components/material-kit-components/Parallax/Parallax.js
+++ src/components/material-kit-components/Parallax/Parallax.js
@@ -13,7 +13,7 @@ const useStyles = makeStyles(styles);
 
 export default function Parallax(props) {
   let windowScrollTop;
-  if (window.innerWidth >= 768) {
+  if (typeof window !== 'undefined' && window.innerWidth && window.innerWidth >= 768) {
     windowScrollTop = window.pageYOffset / 3;
   } else {
     windowScrollTop = 0;
@@ -22,18 +22,20 @@ export default function Parallax(props) {
     "translate3d(0," + windowScrollTop + "px,0)"
   );
   React.useEffect(() => {
-    if (window.innerWidth >= 768) {
+    if (typeof window !== 'undefined' && window.innerWidth && window.innerWidth >= 768) {
       window.addEventListener("scroll", resetTransform);
     }
     return function cleanup() {
-      if (window.innerWidth >= 768) {
+      if (typeof window !== 'undefined' && window.innerWidth && window.innerWidth >= 768) {
         window.removeEventListener("scroll", resetTransform);
       }
     };
   });
   const resetTransform = () => {
-    var windowScrollTop = window.pageYOffset / 3;
-    setTransform("translate3d(0," + windowScrollTop + "px,0)");
+    if (typeof window !== 'undefined' && window.pageYOffset){
+      var windowScrollTop = window.pageYOffset / 3;
+      setTransform("translate3d(0," + windowScrollTop + "px,0)");
+    }
   };
   const { filter, className, children, style, image, small } = props;
   const classes = useStyles();
