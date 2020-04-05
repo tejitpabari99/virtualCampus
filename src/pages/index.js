17b0e2d6f8999fc9724501a2cec3d6402d73de72
tejitpabari99 <tvp2107@columbia.edu>
2020-04-05 19:01:04 +0530

    Changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,21 +8,32 @@ import Parallax from "../components/material-kit-components/Parallax/Parallax.js
 import HomeDescription from '../components/home-sections/HomeDescription'
 import CustomHeader from "../components/CustomHeader";
 import CategoriesSection from '../components/home-sections/CategoriesSection'
+import DesignChallengeDescription from "../components/home-sections/DesignChallengeDescription";
 import styles from "../assets/material-kit-assets/jss/material-kit-react/views/landingPage.js";
 import {makeStyles} from "@material-ui/core/styles";
 
 const useStyles = makeStyles(styles);
 
+const manualSt = makeStyles(() => ({
+  toAll: {
+    fontFamily: "Roboto, Helvetica, Arial, sans-serif", fontWeight: 300, lineHeight: '1.5em',
+    WebkitFontSmoothing:"antialiased", boxSizing: 'inherit'
+  }
+}));
+
 
 export default function IndexPage() {
   const classes = useStyles();
+  const manual = manualSt();
   return (
-    <div style={{background: "#FFFFFF", fontFamily: "Roboto Helvetica Arial sans-serif", fontWeight: 300, lineHeight: '1.5em'}}>
+    <div style={{background: "#FFFFFF"}} className={manual.toAll}>
       <CustomHeader></CustomHeader>
       <Parallax small image={require("../assets/img/campus_graphic.png")} style={{backgroundPosition: "50% 30%"}}>
         {/*<div style={{height:"100%", width:"100%", backgroundColor:"#00000080"}}>*/}
           <div className={classes.container} style={{textAlign:'center', marginTop:'30%'}}>
-                <h1 style={{color:"#f57d20", alignText:'center'}}><strong>Virtual Campus</strong></h1>
+                <h1 style={{color:"#f57d20", alignText:'center', fontSize: '3.3125rem', lineHeight: '1.15em', }} className={manual.toAll}>
+                  <strong>Virtual Campus</strong>
+                </h1>
           </div>
         {/*</div>*/}
       </Parallax>
@@ -30,12 +41,6 @@ export default function IndexPage() {
         <div className={classes.container} id="explore">
           <HomeDescription/>
           <CategoriesSection />
-            <h5 className={classes.description}>
-              Look out for the Virtual Campus Design Challenge 2020 coming up online from April 4th to April 6th, 2020 open to all Columbia/Barnard students.
-              To learn more about it and join, visit our <a href="https://www.facebook.com/events/1020511525011664/">Facebook group</a> and join our <a href=" http://bit.ly/virtual-campus-resources ">Slack channel</a>.
-              Please contact us if you have any more questions.
-              We expect to have many more amazing projects and resources on the website after that time.
-            </h5>
           <div style={{marginBottom: "50px"}}/>
         </div>
       </div>
