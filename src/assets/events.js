f4534cf99bf5c33325daa6b5ea8ed4636e13f71e
tejitpabari99 <tvp2107@columbia.edu>
2020-04-19 04:40:37 +0530

    Lioncraft

--- src/assets/events.js
+++ src/assets/events.js
@@ -4,13 +4,14 @@ const boardGamesImage = require('./img/boardgame_pexels.jpg');
 export default [
   {
     id: 0,
-    title: 'Weekly Board Game Nights',
-    startTime: new Date(2020, 3, 24, 17),
-    endTime: new Date(2020, 3, 24, 19),
-    description: 'Do you miss Hex? Does the word "Catan" bring back fond memories? Join us every week starting every ' +
-      'Friday evening to play online games with friends and form this new virtual community.',
-    tags: ['events'],
-    location: 'https://www.google.com',
-    imgLink: boardGamesImage,
+    title: 'Barnard Campus Launch',
+    startTime: new Date(2020, 3, 19, 17),
+    endTime: new Date(2020, 3, 19, 19),
+    description: 'Our goal is to provide current and former Columbia students with a virtual campus to reunite with ' +
+      'classmates and professors in the Minecraft Virtual World. By working together as a community, ' +
+      'users can help rebuild Columbia in all of its glory, block by block.',
+    tags: ['Minecraft'],
+    location: 'http://lioncraft.nyc/',
+    imgLink: 'http://lioncraft.nyc/assets/img/home.png',
   }
 ]
