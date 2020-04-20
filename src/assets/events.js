4d0405c37c06d1a2a39f59abe386829dcbbcef1a
tejitpabari99 <tvp2107@columbia.edu>
2020-04-20 22:59:59 +0530

    Timezone

--- src/assets/events.js
+++ src/assets/events.js
@@ -5,7 +5,7 @@ const eventLinkUnavailable = 'TBA';
 
 let convertDate = function(date) {
   return new Date(date.getFullYear(), date.getMonth(), date.getDate(), date.getHours(), date.getMinutes())
-}
+};
 
 export default [
   {
@@ -13,10 +13,10 @@ export default [
     title: 'Lioncraft Barnard Launch',
     startTime: DateTime.fromObject({
       year:2020, month:4, day: 19, hour: 17,
-      zone: 'America/New_York'}).toLocal().toJSDate(),
+      zone: 'America/New_York'}) ,
     endTime: DateTime.fromObject({
       year:2020, month:4, day: 19, hour: 19,
-      zone: 'America/New_York'}).toLocal().toJSDate(),
+      zone: 'America/New_York'}) ,
     description: 'Our goal is to provide current and former Columbia students with a virtual campus to reunite with ' +
       'classmates and professors in the Minecraft Virtual World. By working together as a community, ' +
       'users can help rebuild Columbia in all of its glory, block by block.',
@@ -32,10 +32,10 @@ export default [
     title: 'Yom HaShoah Webinar with Sami Steigmann',
     startTime: DateTime.fromObject({
       year:2020, month:4, day: 20, hour: 15,
-      zone: 'America/New_York'}).toLocal().toJSDate(),
+      zone: 'America/New_York'}) ,
     endTime: DateTime.fromObject({
       year:2020, month:4, day: 20, hour: 16,
-      zone: 'America/New_York'}).toLocal().toJSDate(),
+      zone: 'America/New_York'}) ,
     description: 'April 20-21 is the Holocaust Memorial Day in Israel. We are hosting a webinar called ""Yom Hashoah with Sami Steigmann". ' +
       'He is a Holocaust survivor and Motivational Speaker. In the event, he delves into the importance of History, terminology, ' +
       'Israel, and prosperity. ',
@@ -51,10 +51,10 @@ export default [
     title: 'Talking Israel on Campus',
     startTime: DateTime.fromObject({
       year:2020, month:4, day: 20, hour: 20,
-      zone: 'America/New_York'}).toLocal().toJSDate(),
+      zone: 'America/New_York'}) ,
     endTime: DateTime.fromObject({
       year:2020, month:4, day: 20, hour: 21,
-      zone: 'America/New_York'}).toLocal().toJSDate(),
+      zone: 'America/New_York'}) ,
     description: 'We plan to host a webinar with Dalia Zahger (GS \'19); she is an SSI Columbia President Alumna. ' +
       'She going to reflect on her own experience as a former SSI president and going to discuss how to address and ' +
       'respond to questions about Israel on campus.',
@@ -70,10 +70,10 @@ export default [
     title: 'Genderev Social Network Launch',
     startTime: DateTime.fromObject({
       year:2020, month:4, day: 22, hour: 13,
-      zone: 'America/New_York'}).toLocal().toJSDate(),
+      zone: 'America/New_York'}) ,
     endTime: DateTime.fromObject({
       year:2020, month:4, day: 22, hour: 15,
-      zone: 'America/New_York'}).toLocal().toJSDate(),
+      zone: 'America/New_York'}) ,
     description: 'genderev.com is the new social network for members of Genderev at Columbia. ',
     tags: ['Social'],
     website: 'https://genderev.com/',
