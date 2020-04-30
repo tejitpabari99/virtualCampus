a81215e92e804c298b4939168c2c6b8c5dd5b2f8
claire-1 <claire8991@gmail.com>
2020-04-29 18:20:06 -0700

    Add scripts to run/clean up docker

---
+++ run.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+# This script stops all running docker containers before
+# rebuilding them and running them again.
+
+sudo docker-compose down
+sudo docker-compose build
+sudo docker-compose up
---
+++ scripts/clean-up.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# This script cleans up old docker files that are no longer needed.
+# These files accumulate over time and can take up a lot of space. 
+sudo docker volume rm $(sudo docker volume ls -qf dangling=true)
+yes | sudo docker system prune -a 
