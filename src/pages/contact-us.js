ade1c6e1a5a6c9e348c1820085a4a9e4db1e687c
haritipatel <haritipatel2000@gmail.com>
2020-05-06 12:01:46 -0400

    Contact Page Revised

--- src/pages/contact-us.js
+++ src/pages/contact-us.js
@@ -1,8 +1,15 @@
 import React from "react"
 import Template from "../components/template";
-import Card from "../components/material-kit-components/Card/Card.js";
-import CardBody from "../components/material-kit-components/Card/CardBody.js";
+import GridContainer from "../components/material-kit-components/Grid/GridContainer.js";
+import GridItem from "../components/material-kit-components/Grid/GridItem.js";
+import CustomInput from "../components/material-kit-components/CustomInput/CustomInput.js";
+import Button from "../components/material-kit-components/CustomButtons/Button.js";
 import { Helmet } from 'react-helmet'
+import PersonIcon from "@material-ui/icons/Person";
+import EmailIcon from "@material-ui/icons/Email";
+import MessageIcon from "@material-ui/icons/Message";
+import InputAdornment from "@material-ui/core/InputAdornment";
+
 
 const TITLE = 'Contact Us'
 export default function IndexPage() {
@@ -21,20 +28,67 @@ export default function IndexPage() {
               <meta property="og:image:width" content="200" />
               <meta property="og:image:height" content="200" />
             </Helmet>
-            <Card>
-                <CardBody>
-                    <h3 style={{fontFamily:'Poppins, Roboto, Helvetica, Arial, sans-serif'}}><strong>Contact Us</strong></h3><br/>
-                    Our email: <a style={{fontFamily:'Poppins, Roboto, Helvetica, Arial, sans-serif'}} href={'mailto:columbiavirtualcampus@gmail.com'}>columbiavirtualcampus@gmail.com</a>
-                    <br/><br/>
-                    Our Facebook: <a style={{fontFamily:'Poppins, Roboto, Helvetica, Arial, sans-serif'}} href='https://bit.ly/virtual-campus-group'>https://bit.ly/virtual-campus-group</a>
-                    <br/><br/>
-                    Our Slack group: <a style={{fontFamily:'Poppins, Roboto, Helvetica, Arial, sans-serif'}} href='https://bit.ly/virtual-campus-slack'>https://bit.ly/virtual-campus-slack</a>
-                    <br/><br/>
-                    Our design challenge: <a style={{fontFamily:'Poppins, Roboto, Helvetica, Arial, sans-serif'}}href='https://bit.ly/design-challenge-rsvp'>https://bit.ly/design-challenge-rsvp</a>
-                </CardBody>
-                <div style={{minHeight: '30px'}}/>
-            </Card>
-            <div style={{minHeight: '60px'}}/>
+
+                <h3 style={{textAlign:"center", color:"#4284C8", fontFamily:'Poppins, Roboto, Helvetica, Arial, sans-serif'}}><strong>Contact Us</strong></h3><br/>
+                <div style={{textAlign:'center'}}>
+                Our Email: <a style={{textAlign:"center", color:"#4284C8", fontFamily:'Poppins, Roboto, Helvetica, Arial, sans-serif'}} href={'mailto:columbiavirtualcampus@gmail.com'}>columbiavirtualcampus@gmail.com</a>
+                <br/><br/>
+                Our Facebook: <a style={{textAlign:"center", color:"#4284C8", fontFamily:'Poppins, Roboto, Helvetica, Arial, sans-serif'}} href='https://bit.ly/virtual-campus-group'>https://bit.ly/virtual-campus-group</a>
+                </div>
+                <br/>
+
+                <div style={{minHeight: '10px'}}/>
+
+                <div style={{minHeight: '10px'}}/>
+
+                <h3 style={{textAlign:"center", color:"#4284C8", fontFamily:'Poppins, Roboto, Helvetica, Arial, sans-serif'}}><strong>Feedback</strong></h3>
+                <GridContainer>
+                        <GridItem xs={12} sm={12} md={4}>
+                            <CustomInput
+                                labelText="First and Last Name"
+                                id="material"
+                                formControlProps={{
+                                    fullWidth: true
+                                }}
+                                inputProps={{
+                                    endAdornment: (<InputAdornment position="end"><PersonIcon style={{fill: "#4284C8"}}/></InputAdornment>)
+                                }}
+                            />
+                        </GridItem>
+                        <br/>
+                        <GridItem xs={12} sm={12} md={4}>
+                            <CustomInput
+                                labelText="Email"
+                                id="material"
+                                formControlProps={{
+                                    fullWidth: true
+                                }}
+                                inputProps={{
+                                    endAdornment: (<InputAdornment position="end"><EmailIcon style={{fill: "#4284C8"}}/></InputAdornment>)
+                                }}
+                            />
+                        </GridItem>
+                        <br/>
+                        <GridItem xs={12} sm={12} md={4}>
+                            <CustomInput
+                                labelText="Message"
+                                id="material"
+                                formControlProps={{
+                                    fullWidth: true
+                                }}
+                                inputProps={{
+                                    endAdornment: (<InputAdornment position="end"><MessageIcon style={{fill: "#4284C8"}}/></InputAdornment>)
+                                }}
+                            />
+                        </GridItem>
+                  </GridContainer>
+
+                  <div style={{textAlign:'center'}}>
+                  <Button color="vcColor" size="sm"
+                          active={true} target={'_blank'} rel="noopener noreferrer"
+                          href={'mailto:columbiavirtualcampus@gmail.com'}> Submit
+                  </Button>
+                  </div>
         </Template>
     );
 }
