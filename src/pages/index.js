b7d1633db48f8e4ce3e085edb3cfaf60a123261e
gitika-bose <34962222+gitika-bose@users.noreply.github.com>
2020-05-17 16:39:51 -0400

    homepage (#45)
    * Fixed line breaks in GCal and iCal
    
    * Changed layout. Changed button. Added new background. Changed text and position and styling
    
    * Remodified css to get correct positioning
    
    * Fixed margins on black text
    
    * Added host event section
    
    * Fixed spacing on mobile and on desktop.
    
    * Changed the sizing/stretching so it does not looked stretched anymore
    
    * Take one: Showing upcoming events when page loads
    
    * Added npm react detect device and split components for mobile and desktop home page
    
    * Split desktop and mobile home page into components
    
    * Added comments to index.js and added more css to HomeDesktop.js
    
    * Checkpoint
    
    * Added variation for mobile's screen orientation
    
    * Added comments
    
    * Removed unnecessary imports
    
    * Touched up a little
    
    * Added npm installs
    
    * Fixed sizing again
    
    * Pushed upcoming events down to account for android users
    
    * Added more response for desktop version when making window smaller
    
    * Fixed comments
    
    * Fixed logic in index.js and removed redudancies in desktop component's css
    
    * Modified how responsive the page is by using vw and vh and changed the way that upcoming events is positioned <-- Much better and reliable. It was for the screen resizing on desktop. I do not know how much this matters, but I have tried to get the home page to look acceptable for any screen width or height combination that I have
    
    * Renamed css items in desktop version. Fixed comments
    
    * Changed styles
    
    * Added logic for Edge because it does not render right
    
    * Added base styles for edge
    
    * Added styles for edge
    
    * Finalized depriciated edge and explorer css
    
    * Commented out stretched version of the home desktop page. The commented out line is at line 67 in  HomeDesktop.js. If you want the stretched version, then uncomment line 67 and remove line 68 :).
    
    * Commented out stretched version of the home desktop page. The commented out line is at line 67 in  HomeDesktop.js. If you want the stretched version, then uncomment line 67 and remove line 68 :). Added a TODO in index.js
    
    * Fixed IE and Edge browsers by defaulting to mobile. It looks better than it being broke.
    
    * fixed mobile on gatsby serve
    
    * merging homepage redesign
    
    Co-authored-by: James Mastran <jam2454@columbia.edu>
    Co-authored-by: Gitika Bose <gitikabose@gitikas-mbp-2.lan>

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,173 +1,83 @@
 import React from "react"
-import classNames from "classnames";
-import Button from "../components/material-kit-components/CustomButtons/Button.js";
-import HomeEvents from '../components/home/HomeEvents'
-import styles from "../assets/material-kit-assets/jss/material-kit-react/views/landingPage.js";
-import { makeStyles } from "@material-ui/core/styles";
-import landingImage from "../assets/images/home/graphic.png";
-import AddIcon from "@material-ui/icons/Add";
-import {MetaData, CustomTheme, CustomHeader} from "../components";
-const theme = CustomTheme;
+import HomeDesktop from "../components/home/HomeDesktop";
+import HomeMobile from "../components/home/HomeMobile";
+import {isMobile, isTablet, isEdge, isIE} from "react-device-detect";
 
-const useStyles = makeStyles(styles);
+class Index extends React.Component {
 
-const manualSt = makeStyles(() => ({
-  toAll: {
-    [theme.breakpoints.up('xs')]:{
-      fontSize: '2.5rem',
-      lineHeight: '1.25em',
-      textAlign:'center',
-    },
-    [theme.breakpoints.up('sm')]:{
-      fontSize: '4rem',
-      lineHeight: '1.25em',
-      textAlign:'right',
-    },
-    [theme.breakpoints.up('lg')]:{
-      fontSize: '4rem',
-      lineHeight: '1em',
-      textAlign:'right',
-    },
-    color:'#F1945B',
-  },
-  toAllLeft: {
-    [theme.breakpoints.up('xs')]:{
-      marginTop: '30%',
-      marginLeft: 0,
-      textAlign:'center',
-    },
-    [theme.breakpoints.up('sm')]:{
-      marginTop: 'auto',
-      textAlign: 'left',
-      marginLeft: '-8rem',
-    },
-    color:"#BFD8E9",
-  },
-  toAll2:{
-    margin:"0px"
-  },
-  button:{
-    boxShadow: 'none',
-    borderRadius: 30,
-    fontSize: '1.1rem',
-    width: 200,
-    border: '1px solid #4284C8',
-    backgroundColor: 'white',
-    paddingTop: 10,
-    paddingBottom: 10,
-    "&:hover": {
-      backgroundColor: '#4284C8',
-    },
-    "&:hover span": {
-      color: 'white !important',
-    },
-  },
-  buttonSpan:{
-    color: '#4284C8',
-  },
-  text:{
-    [theme.breakpoints.up('xs')]:{
-      marginTop: '0',
-      float:'right',
-      width: '100%',
-      textAlign:'center'
-    },
-    [theme.breakpoints.up('sm')]: {
-      margin: '0',
-      float:'right',
-      marginRight: '3%',
-      marginTop: "10%" ,
-      textAlign:'right',
-      width: 'auto'
-    },
-    [theme.breakpoints.up('md')]:{
-      margin: 0,
-      float:'right',
-      marginRight: '3%',
-      marginTop: "10%" ,
-      textAlign:'right'
-    },
-    [theme.breakpoints.up('lg')]:{
-      margin: 0,
-      float:'right',
-      marginRight: '3%',
-      marginTop: "10%" ,
-      textAlign:'right'
+    constructor(props) {
+        super(props);
+        this.state = { width: 0, height: -1 };
+        this.updateWindowDimensions = this.updateWindowDimensions.bind(this);
     }
-  },
-  landing: {
-    [theme.breakpoints.up('xs')]:{
-      background: `url(${landingImage})`,
-      backgroundSize: '200%',
-      backgroundRepeat: 'no-repeat',
-      backgroundPosition: '80% 100%',
-    },
-    [theme.breakpoints.up('sm')]:{
-      background: `url(${landingImage})`,
-      backgroundSize: '100%',
-      backgroundRepeat: 'no-repeat',
-      backgroundPosition: 'left bottom',
-    },
-    [theme.breakpoints.up('md')]:{
-      background: `url(${landingImage})`,
-      backgroundSize: '70%',
-      backgroundRepeat: 'no-repeat',
-      backgroundPosition: 'left center',
-    },
-    [theme.breakpoints.up('lg')]: {
-      background: `url(${landingImage})`,
-      backgroundSize: '70%',
-      backgroundRepeat: 'no-repeat',
-      backgroundPosition: 'left center',
-    },
-  },
-  addNewButton:{
-    boxShadow:"none",
-    fontSize: 20,
-  },
-}));
+
+    componentDidMount() {
+        this.updateWindowDimensions();
+        window.addEventListener('resize', this.updateWindowDimensions);
+    }
+
+    componentWillUnmount() {
+        window.removeEventListener('resize', this.updateWindowDimensions);
+    }
+
+    updateWindowDimensions() {
+        this.setState({ width: window.innerWidth, height: window.innerHeight });
+    }
+
+    // TODO: Remove duplicate code in HomeDesktop.js and HomeMobile.js
+    render() {
+
+        if (this.state.height === -1) {
+            return (
+                <div>
+                    <HomeDesktop/>
+                </div>
+            );
+        }
+        {/* For mobile's screen orientation update */}
+        const isLandscape = this.state.width > this.state.height ? true : false;
+
+        {/* If Tablet:
+            If in portrait, do mobile component
+            else render desktop
+            */}
+        if (isTablet) {
+            if (isLandscape) {
+                return (
+                <div>
+                    <HomeDesktop/>
+                </div>
+                );
+            } else {
+                return (
+                    <div>
+                        <HomeMobile isLandscape={isLandscape}/>
+                    </div>
+                );
+            }
 
 
-export default function IndexPage() {
-  const classes = useStyles();
-  const manual = manualSt();
-  return (
-    <div style={{background: "white"}}>
-      <MetaData title={'Columbia Virtual Campus'}/>
-      <CustomHeader active={''} brand={''}></CustomHeader>
-      <div className={classNames(manual.landing)} style={{height:"100vh", width:"100%"}}>
-        <div className={classNames(manual.text)}>
-          <h1 className={classNames(manual.toAll, manual.toAllLeft)}>
-            COLUMBIA
-          </h1>
-          <h1 className={manual.toAll}>
-            VIRTUAL CAMPUS
-          </h1>
-          <Button
-            href="/events"
-            className={classNames(manual.button)}
-          >
-            <span className={manual.buttonSpan}>Explore Events</span>
-          </Button>
-        </div>
-      </div>
-      <div className={classNames(classes.main)} style={{textAlign:'left'}}>
-        <div className={classes.container} id="explore">
-          <div style={{ marginTop: "100px" }}>
-          <h3 style={{ textAlign: "center", color: "#4284C8", fontSize: "30px" }}
-              className={classes.toAll2}> UPCOMING EVENTS </h3>
-          <div style={{textAlign:'center'}}>
-            <Button color="vcColor" size="sm" className={manual.addNewButton}
-                    style={{marginTop: 20}}
-                    active={true} target={'_blank'} rel="noopener noreferrer"
-                    href={'https://forms.gle/fzKvSZqkAVNN6cHY6'}> <AddIcon/> Add New Event
-            </Button>
-          </div>
-          <HomeEvents/>
-          </div>
-          <div style={{marginBottom: "100px"}}/>
-        </div>
-      </div>
-    </div>
-    )
-};
+        {/* For mobile component : IE or Edge must go to mobile since they do not support all css */}
+        } else if (isMobile || (isLandscape === false && this.state.height > 700) || isIE || isEdge) {
+                return (
+                    <div>
+                        <HomeMobile isLandscape={isLandscape}/>
+                    </div>
+                );
+
+
+        {/* Else: desktop: isBrowser
+            If screen is full size and not weirdly shape: render desktop version
+            Else render mobile version (see above)
+            */}
+        } else {
+            return (
+                <div>
+                    <HomeDesktop />
+                </div>
+            );
+        }
+    }
+}
+
+export default Index;
