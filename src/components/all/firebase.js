45ab60a466a5fef15d84400f48478f74fd415afd
Sharon Jin <sharonjin@Sharons-MBP.home>
2020-05-31 19:27:53 -0400

    deleted

--- src/components/all/firebase.js
+++
@@ -1,8 +0,0 @@
-import firebase from 'firebase/app';
-import "firebase/auth";
-import "firebase/firestore";
-import firebaseConfig from '../../../firebaseConfig'
-
-firebase.initializeApp(firebaseConfig);
-
-export default firebase;
