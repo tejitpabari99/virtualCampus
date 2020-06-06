dc7ee87bc9c774bf084c72b73ffcdb9de11d78da
mastranj <51935674+mastranj@users.noreply.github.com>
2020-06-06 12:52:24 -0400

    Updated README.md to Claire's and added a small new line about adding specific functions to firebase. Merged in Alistair's code and updated it in a small way by changing the positioning.

--- src/pages/contact-us.js
+++ src/pages/contact-us.js
@@ -35,7 +35,7 @@ class contactUs extends React.Component {
       text: this.state.text
     };
 
-    Axios.post("https://us-central1-columbia-virtual-campus.cloudfunctions.net/sendEmail", data)
+    /*Axios.post("https://us-central1-columbia-virtual-campus.cloudfunctions.net/sendEmail", data)
       .then(res => {
         this.setState({ activityIndicatory: false });
         this.setState({ feedbackSubmit: true });
@@ -43,7 +43,7 @@ class contactUs extends React.Component {
       })
       .catch(error => {
         console.log(error);
-      });
+      });*/
 
   }
 
@@ -67,7 +67,7 @@ class contactUs extends React.Component {
         <div style={{ backgroundColor: "white" }}>
           <div style={{ backgroundColor: "white" }}>
             <CustomHeader active={"schedule"} brand={"VIRTUAL CAMPUS"}/>
-            <div style={{marginTop: '25%', marginLeft:'50%'}}>
+            <div style={{marginTop: '3%', marginLeft:'49%'}}>
               <CircularProgress />
             </div>
           </div>
