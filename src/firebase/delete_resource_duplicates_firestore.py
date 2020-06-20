5d80e4672679dc8c79ca667bab25d8a10ca8bfc9
luizvalle <lld2131@columbia.edu>
2020-06-20 12:57:37 -0400

    Modified delete duplicate script to use resource module

--- src/firebase/delete_resource_duplicates_firestore.py
+++ src/firebase/delete_resource_duplicates_firestore.py
@@ -3,54 +3,8 @@ import sys
 import firebase_admin
 from typing import List, Dict
 from firebase_admin import firestore
+from resource import Resource, Links
 from firebase_admin import credentials
-
-class Resource(object):
-    
-    def __init__(self, resource_dict):
-        self._title = resource_dict["title"]
-        self._reviewed = resource_dict["reviewed"]
-        self._description = resource_dict["description"]
-        self._img = resource_dict["img"]
-        self._category = resource_dict["category"]["category"]
-        self._tags = resource_dict["category"]["tags"]
-        self._links = resource_dict["links"]
-
-    def __eq__(self, other) -> bool:
-        if not isinstance(other, Resource):
-            return False
-        return self.title == other.title or self.links == other.links
-    
-    def __hash__(self):
-        return hash((self.title, frozenset(self.links.items())))
-
-    @property
-    def title(self) -> str:
-        return self._title
-
-    @property
-    def reviewed(self) -> bool:
-        return self._reviewed
-
-    @property
-    def description(self) -> str:
-        return self._description
-
-    @property
-    def img(self) -> str:
-        return self._img
-
-    @property
-    def category(self) -> str:
-        return self._category
-    
-    @property
-    def tags(self) -> List[str]:
-        return self._title
-
-    @property
-    def links(self) -> Dict[str,str]:
-        return self._links
 
 
 def main():
@@ -82,7 +36,7 @@ def remove_duplicates(docs):
     total = removed = 0
     log("Delete duplicates:")
     for doc in docs:
-        resource = Resource(doc.to_dict())
+        resource = Resource.from_dict(doc.to_dict())
         if resource in seen:
             doc.reference.delete()
             log(f"\tDocument with title \"{resource.title}\"")
