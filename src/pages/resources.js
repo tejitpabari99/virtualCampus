6f5d574bc0a8460f12041bb3ea2dbbbc01afb00a
Sharon <sharonjin17@gmail.com>
2020-06-21 09:24:20 -0400

    Update resources.js

--- src/pages/resources.js
+++ src/pages/resources.js
@@ -1,10 +1,22 @@
 import React from "react"
 import Template from "../components/all/Template";
+import AddIcon from "@material-ui/icons/Add";
+import Button from "../components/material-kit-components/CustomButtons/Button.js";
+import {MetaData, CustomButton, Title} from "../components";
+import ResourcesList from "../components/resources/ResourcesList";
+import ResourcesFeatured from "../components/resources/ResourcesFeatured";
 
-import {ResourcesList, Title} from "../components";
-import ResourcesFeatured from "../components/resources/featured/ResourcesFeatured";
+const useStyles = () => ({
+    addNewButton:{
+        boxShadow:"none",
+        fontSize: 15,
+        marginLeft:'auto',
+        marginRight:'auto'
+    }
+});
 
 export default function Resources() {
+    const classes = useStyles();
     return (
         <Template active={'resources'} title={'Resources'}>
           <Title color={'blue'}>Resources</Title>
