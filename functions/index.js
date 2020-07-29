2699eb5e431b21d48295280ebff37a58820126a2
Kevin Mao <66439901+KevinMicrosoft@users.noreply.github.com>
2020-07-29 11:33:41 -0400

    Mock tech interview calendar adjustments (#81)
    * Add max weekly interviews checks
    
    * Coding interview page UI adjustments
    
    * Fixed weekly check in rendering calendar
    
    * Added loading bar to calendar
    
    * increase signup cap to 3
    
    * Fix resume link overflow issue
    
    * Oepn resume link in new tab

--- functions/index.js
+++ functions/index.js
@@ -95,8 +95,8 @@ exports.bookEvent = functions.https.onRequest(async (req, res) => {
       const checkAttendance = await db.collection('technical')
         .where("attendee_email", "==", email)
         .where("available", "==", false).get();
-      if (checkAttendance.size > 1){
-        return res.status(412).send("You can only sign up for a maximum of 2 sessions!");
+      if (checkAttendance.size > 2){
+        return res.status(412).send("You can only sign up for a maximum of 3 sessions!");
       }
 
       const attendeeText = `Dear ${name},<br/><br/>
@@ -212,6 +212,7 @@ exports.scheduleEvents = functions.https.onRequest(async (req, res) => {
         start_time.setFullYear(temp_year);
 
         // CAREFUL: USER CANNOT PASS IN END TIME BEFORE START TIME
+        // THIS LOGIC DOESNT WORK
         if (start_time.getHours() > end_time.getHours()){
           end_time.setDate(temp_date+1);
         } else {
