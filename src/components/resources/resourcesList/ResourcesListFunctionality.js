b70416ab885da00e9390958b08cc74b67046e80a
Sharon <sharonjin17@gmail.com>
2020-08-01 14:33:47 -0400

    fixed tags bug, loading from Firestore

--- src/components/resources/resourcesList/ResourcesListFunctionality.js
+++ src/components/resources/resourcesList/ResourcesListFunctionality.js
@@ -54,7 +54,6 @@ class ResourcesListFunctionality extends React.Component {
       event: {}
     };
     this.getResources();
-    //this.handleChange = this.handleChange.bind(this);
   }
 
   /**
@@ -64,8 +63,6 @@ class ResourcesListFunctionality extends React.Component {
   async getResources() {
     let approvedResourcesDict = {};
     let allResources = [];
-    let approvedTagsDict = {};
-    let approvedResources = [];
     try{
       let db = firebase.firestore();
       // let approvedResources = await db.collection("resources").where("reviewed", "==", true).get();
@@ -78,9 +75,10 @@ class ResourcesListFunctionality extends React.Component {
         arr.push(key);
       });
 
+      // make dictionary of category -> list of corresponding resources
       for (let i = 0; i < arr.length; i++)
       {
-
+        let categoryResources = [];
         // changed the loop to retrieve from resource by iterating through each category
         let name = arr[i];
         let template = "/resource/" + name + "/" + name;
@@ -88,21 +86,17 @@ class ResourcesListFunctionality extends React.Component {
 
         all_reviewed.forEach(doc =>
         {
-          approvedResources.push(doc.data());
+          categoryResources.push(doc.data());
+          allResources.push(doc.data());
         });
 
+        approvedResourcesDict[this.toTitleCase(name)] = categoryResources;
       }
+      approvedResourcesDict["All Resources"] = allResources;
 
-      if(approvedResources){
-        allResources = approvedResources;
-        approvedResourcesDict = this.makeDisplayResources(allResources);
-        approvedTagsDict = this.makeDisplayTags(allResources);
-      }
       this.setState({
         activityIndicator: false,
         resourcesDict: approvedResourcesDict,
-        resourcesDisplay: allResources,
-        tagsDict: approvedTagsDict
       });
       this.setDisplay('All Resources');
     }
@@ -111,76 +105,13 @@ class ResourcesListFunctionality extends React.Component {
     }
   }
 
-
-  /**
-  * Creates mapping of category to corresponding resources
-  * @param  {[]} resources: List of resources
-  * @return {Dict} res: Dictionary with categories (Social, All Resources, etc) as keys,
-    * list of corresponding resources as values
-  */
-  makeDisplayResources(resources) {
-    let res = {};
-    res['All Resources'] = resources;
-    for (let i = 0; i < resources.length; i += 1) {
-      let ele = resources[i];
-      let key = this.toTitleCase(ele['category']['category']);
-      if (key in res) {
-        res[key].push(ele)
-      }
-      else {
-        res[key] = [ele]
-      }
-    }
-    return res;
-  }
-
-  /**
-  * Creates nested mapping of category to tag to corresponding resources
-  * @param  {[]} resources: List of resources
-  * @return {Dict} res: Nested dictionary with categories (Social, All Resources, etc) as keys of outer dict,
-    * inner dict as values of outer dict,
-    * tags (BLM, Job, etc) as keys of inner dict,
-    * list of corresponding resources as values of inner dict
-  */
-  makeDisplayTags(resources) {
-    let res = {'All Resources':{}};
-    for (let i = 0; i < resources.length; i += 1) {
-      let ele = resources[i];
-      let key = this.toTitleCase(ele['category']['category']);
-      let tag = ele['category']['tags'];
-
-      for(let j = 0; j < tag.length; j++){
-        let tagName = this.toTitleCase(tag[j]);
-        // if category not added yet, add tag and resource
-        if (!(key in res)) {
-          res[key] = {};
-          res[key][tagName] = [ele];
-          res['All Resources'][tagName] = [ele];
-        }
-        // if category is already added
-        else{
-          // if tag exists, add resource
-          if(res[key][tagName]){
-              res[key][tagName].push(ele);
-              res['All Resources'][tagName].push(ele);
-          }
-          // if tag doesn't exist, add tag and resource
-          else{
-            res[key][tagName] = [ele];
-            res['All Resources'][tagName] = [ele]
-          }
-        }
-      }
-    }
-    return res;
-  }
-
   /**
   * Make first letter of each word in each category uppercase (e.g. jobs/ internships -> Jobs/ Internships)
   * @param  {String} str: Category name
-  * @return {[]} res: Category name with the first letter of each word capitalized
+  * @return {String} res: Category name with the first letter of each word capitalized
   */
   toTitleCase(str) {
+    str = str.replace("_", "/ ");
     return str.replace(/\w\S*/g, function(txt){
       return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
     });
@@ -193,52 +124,78 @@ class ResourcesListFunctionality extends React.Component {
   * @param  {String} category: Category name
   */
   setDisplay(category) {
+    let resources = this.state.resourcesDict[category];
+    let tagsDict = this.makeTags(resources);
     this.setState({
-      resourcesDisplay: this.state.resourcesDict[category],
+      resourcesDisplay: resources,
       description: Descriptions[category],
       category: category,
-      tagsDisplay: Object.keys(this.state.tagsDict[category]),
+      tagsDict: tagsDict,
+      tagsDisplay: Object.keys(tagsDict),
       tagsResourcesDisplay: {},
       selection: 1
     });
   }
 
   /**
+  * Make tags based on the resources that are currently displayed
+  * @param  {[]} resources: Category name
+  */
+  makeTags(resources) {
+    let res = {};
+
+    for(let i = 0; i < resources.length; i++){
+      let ele = resources[i];
+      let tags = ele['category']['tags'];
+
+      for(let j = 0; j < tags.length; j++){
+        let tagName = this.toTitleCase(tags[j]);
+        // if tag exists, add resource
+        if(res[tagName]){
+            res[tagName].push(ele);
+        }
+        // if tag doesn't exist, add tag and resource
+        else{
+          res[tagName] = [ele];
+        }
+      }
+    }
+    return res
+  }
+
+  /**
   * Add selected tags (keys) along with list of corresponding resources (values) to tagsResourcesDisplay dict
   * Takes all resources present in the dict, renders list of resources, sets resource display to these resources
-  * @param  {String} category: Currently selected category
   * @param  {String} tag: Selected tag
   */
-  setTagDisplay(category, tag) {
-    this.state.tagsResourcesDisplay[tag] = this.state.tagsDict[category][tag];
-    this.renderTagDisplay(category);
+  setTagDisplay(tag) {
+    this.state.tagsResourcesDisplay[tag] = this.state.tagsDict[tag];
+    this.renderTagDisplay();
   }
 
   /**
   * Delete selected tags (keys) along with list of corresponding resources (values) from tagsResourcesDisplay dict
   * Takes remaining resources present in the dict, renders list of resources, sets resource display to these resources
-  * @param  {String} category: Currently selected category
   * @param  {String} tag: Deselected tag
   */
-  deleteTagDisplay(category, tag) {
+  deleteTagDisplay(tag) {
     delete this.state.tagsResourcesDisplay[tag];
-    this.renderTagDisplay(category)
+    this.renderTagDisplay();
   }
 
   /**
   * Takes all resources present in the dict, renders list of resources, sets resource display to these resources
   * If no resources present in dict (all tags deselected), set resources display to all resources in the category
-  * @param  {String} category: Category that's been selected
   */
-  renderTagDisplay(category) {
-    let allResources = [];
-    for(let key in this.state.tagsResourcesDisplay){
-      let resourceList = this.state.tagsResourcesDisplay[key];
-      allResources.push(...resourceList);
-    }
+  renderTagDisplay() {
+    let allResources = [].concat.apply([], Object.values(this.state.tagsResourcesDisplay));
     allResources = Array.from(new Set(allResources));
-    if(allResources.length == 0){
-      this.setState({ resourcesDisplay: this.state.resourcesDict[category]}, function () {
+
+    if(allResources.length === 0){
+      let tags = Object.values(this.state.tagsDict);
+      allResources = [].concat.apply([], tags);
+      allResources = Array.from(new Set(allResources));
+      this.setState({ resourcesDisplay: allResources}, function () {
         this.handleChange(this.state.event);
       });
     }
@@ -254,13 +211,14 @@ class ResourcesListFunctionality extends React.Component {
   * @param  {String} val: Query that's typed into the search bar
   */
   searchFunc(val) {
-    let res = [];
-    let allResources = this.state.resourcesDict['All Resources'];
+    let resources = [];
+    let category = this.state.category;
+    let allResources = this.state.resourcesDict[category];
     let error = "";
     if(!val || val.length === 0){
-      res = allResources;
+      resources = allResources;
     }
-    else if (val.length<=2) {
+    else if (val.length <= 2) {
       error = "ERROR: Search term must be more than 2 characters";
     }
     else {
@@ -273,19 +231,19 @@ class ResourcesListFunctionality extends React.Component {
       let output = fuse.search(val);
 
       for (let i=0; i<output.length; i+=1){
-          res.push(output[i]['item']);
+          resources.push(output[i]['item']);
       }
-      if(output.length == 0){
+      if(output.length === 0){
         error = "No results found";
       }
     }
+    let tagsDict = this.makeTags(resources);
     this.setState({
-      resourcesDisplay: res,
+      resourcesDisplay: resources,
       activityIndicator: false,
-      category: "All Resources",
-      description: "Resources that promote career, foster health, encourage social connection, support basic needs, and raise awareness of COVID.",
-      tagsDisplay: Object.keys(this.state.tagsDict['All Resources']),
-      searchError: error
+      searchError: error,
+      tagsDict: tagsDict,
+      tagsDisplay: Object.keys(tagsDict)
     }, function () {
       this.handleChange(this.state.event);
     });
@@ -296,8 +254,8 @@ class ResourcesListFunctionality extends React.Component {
   * @param  event: Received from <Search> element that has the value of the filter sort
   */
   handleChange = (event, index, value) => { 
-    //alphabetical sort
-    if(event.target!== undefined && event.target.value === 2){
+    // alphabetical sort
+    if (event.target!== undefined && event.target.value === 2){
       let array = this.state.resourcesDisplay;
       array.sort(function(a, b){
         let titleA=a.title.toLowerCase(), titleB=b.title.toLowerCase();
@@ -310,9 +268,9 @@ class ResourcesListFunctionality extends React.Component {
         return 0;
       });
       this.setState({
+        event: event,
         resourcesDisplay: array,
-        selection: event.target.value,
-        event: event
+        selection: event.target.value
       });
     }
 
